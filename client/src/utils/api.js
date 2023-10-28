82e02f4da35069ba1a962208612b89c23af19d78
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-10-29 05:24:26 +0530

    baseUrl update

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "https://full-stack-real-estate-youtube.vercel.app/api",
+  baseURL: "https://project-mern-deploy.vercel.app/api",
 });
 
 export const getAllProperties = async () => {
